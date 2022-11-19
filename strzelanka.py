c615cf876dacbcec86d4b7021a5d7d9be0c8d7c8
Wojciech Radzimowski <wojciech.radzimowski.99@gmail.com>
2022-11-19 16:04:53 +0100

    shortened code

--- strzelanka.py
+++ strzelanka.py
@@ -1,25 +1,23 @@
 import sys
-elements = open(sys.argv[1], "r", encoding="utf-8").readlines()
 user = ""
-albums = {}
+albums_points = {}
 albums_individual_votes = {}
-for element in elements:
+for element in open(sys.argv[1], "r", encoding="utf-8").readlines():
     if "-" not in element:
         user = element.strip()
     else:
         album = element.split(".")[-1].strip()
         points = int(element.split(".")[0])
-        if album in albums:
-            albums[album] += points
+        if album in albums_points:
+            albums_points[album] += points
         else:
-            albums[album] = points
+            albums_points[album] = points
             albums_individual_votes[album] = {}
         albums_individual_votes[album][user] = points
 template = "%place%. %points% pkt - **%album%** _(%users%)_"
-albums_list = sorted(albums.items(), key=lambda item: item[1])
-length = len(albums)
+albums_list = sorted(albums_points.items(), key=lambda item: item[1])
 for index, album in enumerate(albums_list):
-    entry = template.replace("%place%", str(length - index)).replace("%points%", str(album[1])).replace("%album%", album[0])
+    entry = template.replace("%place%", str(len(albums_points) - index)).replace("%points%", str(album[1])).replace("%album%", album[0])
     users_list = []
     for user, points in albums_individual_votes[album[0]].items():
         users_list.append(user + " - " + str(points))
